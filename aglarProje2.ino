6ffbb06948fdd6942efe6ecf78ef164161a8280a
gulzade <32436596+gulzade@users.noreply.github.com>
2019-05-01 16:41:08 +0300

    Update aglarProje2.ino
    engine codes edited

--- aglarProje2.ino
+++ aglarProje2.ino
@@ -1,67 +1,93 @@
-int DonmeHizi = 175;
-const int sagileri = 9;
-const int saggeri = 8;
-const int solileri = 12;
-const int solgeri = 13;
-const int solenable = 11;
-const int sagenable = 10;
-
-void ileri(int hiz){
-  
-  analogWrite(sagenable, hiz); 
-  digitalWrite(sagileri,HIGH); 
-  digitalWrite(saggeri,LOW); 
-
-  analogWrite(solenable, hiz); 
-  digitalWrite(solileri, HIGH); 
-  digitalWrite(solgeri,LOW);
-}
-
-void sagaDon(int hiz){
-  analogWrite(sagenable, hiz); 
-  digitalWrite(sagileri,LOW); 
-  digitalWrite(saggeri,HIGH); 
-
-  analogWrite(solenable, hiz); 
-  digitalWrite(solileri, HIGH); 
-  digitalWrite(solgeri,LOW); 
-}
-
-void solaDon(int hiz){
-  analogWrite(sagenable, hiz); 
-  digitalWrite(sagileri,HIGH);
-  digitalWrite(saggeri,LOW); 
-
-  analogWrite(solenable, hiz); 
-  digitalWrite(solileri, LOW); 
-  digitalWrite(solgeri,HIGH); 
-}
-
-void geri(int hiz){
-  analogWrite(sagenable, hiz); 
-  digitalWrite(sagileri,LOW); 
-  digitalWrite(saggeri, HIGH); 
-
-  analogWrite(solenable, hiz);
-  digitalWrite(solileri, LOW);/
-  digitalWrite(solgeri, HIGH);
-}
-
-void dur()
-{
-  
-  digitalWrite(sagileri, HIGH);
-  digitalWrite(saggeri, HIGH);
-  digitalWrite(solileri, HIGH);
-  digitalWrite(solgeri, HIGH);
-}
-
+#define MotorR1 11
+#define MotorR2 12
+#define MotorRE 13
+#define MotorL1 4
+#define MotorL2 5
+#define MotorLE 3
+int state; //Bluetooth cihazından gelecek sinyalin değişkeni
 void setup() {
-  // put your setup code here, to run once:
-
+  pinMode(MotorR1, OUTPUT);
+  pinMode(MotorR2, OUTPUT);
+  pinMode(MotorRE, OUTPUT);
+  pinMode(MotorL1, OUTPUT);
+  pinMode(MotorL2, OUTPUT);
+  pinMode(MotorLE, OUTPUT);
+  // 9600 baud hızında bir seri port açalım
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+  //Gelen veriyi 'state' değişkenine kaydet
+  if (Serial.available() > 0) {
+    state = Serial.read();
+
+    if (state == 10) { //ileri git
+      forward();
+    }
+
+    else if (state == 20) { //geri git
+      back();
+    }
+    else if (state == 30) { //sola git
+      turnLeft();
+    }
+    else if (state == 40) { //sağa git
+      turnRight();
+    }
+    else {
+      Stop();
+    }
+  }
+}
+void Stop() {
+  digitalWrite(MotorR1, LOW);
+  digitalWrite(MotorR2, LOW);
+  digitalWrite(MotorRE, 150);
+
+  digitalWrite(MotorL1, LOW);
+  digitalWrite(MotorL2, LOW);
+  digitalWrite(MotorLE, 150);
+
+}
+void back() {
+  digitalWrite(MotorR1, LOW);
+  digitalWrite(MotorR2, HIGH);
+  digitalWrite(MotorRE, 150);
+
+  digitalWrite(MotorL1, LOW);
+  digitalWrite(MotorL2, HIGH);
+  digitalWrite(MotorLE, 150);
+
+}
+void forward() {
+  digitalWrite(MotorR1, HIGH);
+  digitalWrite(MotorR2, LOW);
+  digitalWrite(MotorRE, 150);
+
+  digitalWrite(MotorL1, HIGH);
+  digitalWrite(MotorL2, LOW);
+  digitalWrite(MotorLE, 150);
+
+}
+void turnRihgt() {
+  digitalWrite(MotorR1, LOW);
+  digitalWrite(MotorR2, HIGH);
+  digitalWrite(MotorRE, 150);
+
+  digitalWrite(MotorL1, HIGH);
+  digitalWrite(MotorL2, LOW);
+  digitalWrite(MotorLE, 150);
+
+}
+void turnLeft() {
+  digitalWrite(MotorR1, HIGH);
+  digitalWrite(MotorR2, LOW);
+  digitalWrite(MotorRE, 150);
+
+  digitalWrite(MotorL1, LOW);
+  digitalWrite(MotorL2, HIGH);
+  digitalWrite(MotorLE, 150);
 
 }
